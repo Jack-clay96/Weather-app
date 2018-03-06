172a437f6a04e0cdbee1c041ad2d4346a352b13b
Jack-clay96 <32362918+Jack-clay96@users.noreply.github.com>
2018-03-06 13:47:09 +0000

    working async

--- js/index.js
+++ js/index.js
@@ -7,14 +7,21 @@ $(document).on('pagecreate', '#feedPage', function(event) {
 	var xmlhttp=new XMLHttpRequest();
 	//xmlhttp.open("GET", feedURL, false); //Locks app until recieves request back from server
     
-    xhttp.onreadystatechange = function() {
+    xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("#myTemplate").innerHTML = this.responseText;
-             //var weather= JSON.parse(xmlhttp.responseText);
+            var weather= JSON.parse(xmlhttp.responseText);
+            	//Define Ractive binding
+	       var ractive = new Ractive({
+    	   el: 'container', <!-- where -->
+    	   template: '#myTemplate', <!-- how -->
+    	   data: { weather : weather.consolidated_weather } <!-- what - specify the list of weather reports using dot notation-->
+	   });
         }
+        
+        
     };
             
-    xhttp.open("GET", feedURL, true); //Allows user to use app while getting request back
+    xmlhttp.open("GET", feedURL, true); //Allows user to use app while getting request back
 	xmlhttp.send();
 		
 	// parse the resulting JSON into Javascript Data Object 
@@ -22,12 +29,5 @@ $(document).on('pagecreate', '#feedPage', function(event) {
 	// http://json.parser.online.fr/ 
 	//var weather= JSON.parse(xmlhttp.responseText); //HTTP GET request - using XMLHttpRequest
 	
-	
-	//Define Ractive binding
-	var ractive = new Ractive({
-    	el: 'container', <!-- where -->
-    	template: '#myTemplate', <!-- how -->
-    	data: { weather : weather.consolidated_weather } <!-- what - specify the list of weather reports using dot notation-->
-	});
 	
 });
